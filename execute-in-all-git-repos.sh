5e0b55c233a15a6e94474a31c912f8b7429a92a0
morfeush22 <patro.kacper@gmail.com>
2021-07-14 20:10:30 +0200

    some git service commands

---
+++ execute-in-all-git-repos.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=SC1090
+source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/commons.sh
+source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/b-log.sh
+
+LOG_LEVEL_WARN
+
+main() {
+  local all_git_repos_root_dir="$1"
+
+  ls "${all_git_repos_root_dir}"
+
+  for git_repo_subdir in $(find "${all_git_repos_root_dir}" -type d -name ".git"); do
+    local git_repo_main_dir="${git_repo_subdir%/.git}"
+
+    pushd "${git_repo_main_dir}" || return
+
+    ${WHAT}
+
+    popd
+  done
+}
+
+main "$@"
---
+++ polyrepo-git-bootstrap.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=SC1090
+source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/commons.sh
+source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/b-log.sh
+
+LOG_LEVEL_WARN
+
+main() {
+  local polyrepo_git_source_dir="$1"
+  local polyrepo_git_dest_dir="$2"
+
+  if [[ ! -d "${polyrepo_git_source_dir}/.git" ]]; then
+    pushd "${polyrepo_git_source_dir}" || return
+
+    git init
+    git add -A
+    git commit -m "init"
+
+    popd || return
+  fi
+
+  git clone --bare "${polyrepo_git_source_dir}" "${polyrepo_git_dest_dir}"
+}
+
+main "$@"
