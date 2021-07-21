65d41d77d53111d8d44d7c419f8412b1268634d5
morfeush22 <patro.kacper@gmail.com>
2021-07-21 22:44:20 +0200

    monorepo scripts

---
+++ scripts_monorepo/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+make all WHAT=staging/src/k8s.io/apiextensions-apiserver
+make all WHAT=staging/src/k8s.io/kube-aggregator
+make all WHAT=cmd/kube-apiserver
+make all WHAT=cmd/kube-controller-manager
+make all WHAT=cmd/kubectl
+make all WHAT=cmd/kubectl-convert
+make all WHAT=cmd/kubelet
+make all WHAT=cmd/kube-proxy
+make all WHAT=cmd/kube-scheduler
+make all WHAT=cluster/gce/gci/mounter
